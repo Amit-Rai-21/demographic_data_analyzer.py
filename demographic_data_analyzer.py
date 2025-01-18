51ac9af8803fe1f4b8f3f90495862339279f82af
Amit-Rai-21 <ic.amitrai153@gmail.com>
2025-01-18 05:58:29 +0545

    Update demographic_data_analyzer.py

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -6,7 +6,7 @@ race_count = df['race'].value_counts()
 print(race_count)
 
 average_age_men = df[df['sex'] == 'Male']['age'].mean()
-print(f"\nThe average age of men is {average_age_men:}")
+print(f"\nThe average age of men is {average_age_men:.0f}")
 
 total = df['education'].count()
 bachelors_count = df[df['education'] == 'Bachelors']['education'].count()
